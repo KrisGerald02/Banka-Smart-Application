448028b3237f642795d2d875cfe1c60c109cf0be
Kristel Geraldine Villalta Porras <165867799+krisgeraldine@users.noreply.github.com>
2024-06-21 03:47:08 -0600

    Update Mod_Seg.cpp

--- Módulo Seguridad /Mod_Seg.cpp
+++ Módulo Seguridad /Mod_Seg.cpp
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+// Función para generar una contraseña aleatoria
+void generarContrasena(char* contrasena, int longitud) {
+    const char caracteres[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    int numCaracteres = sizeof(caracteres) - 1;
+    
+    srand(time(0)); // Inicializar la semilla para la aleatoriedad
+    
+    for (int i = 0; i < longitud; ++i) {
+        contrasena[i] = caracteres[rand() % numCaracteres];
+    }
+    
+    contrasena[longitud] = '\0'; // Añadir el carácter nulo al final
+}
+
+int main() {
+    char usuario[50];
+    int longitudContrasena = 12;
+    char contrasena[longitudContrasena + 1];
+    
+    printf("Introduce el nombre del usuario: ");
+    scanf("%49s", usuario);
+    
+    generarContrasena(contrasena, longitudContrasena);
+    
+    printf("Usuario: %s\n", usuario);
+    printf("Contrasena generada: %s\n", contrasena);
+    
+    return 0;
+}
