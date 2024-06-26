75dae269c03dd8a6369673516be7bc41cc0cf657
Kristel Geraldine Villalta Porras <165867799+krisgeraldine@users.noreply.github.com>
2024-06-26 16:52:01 -0600

    Update acc_handler.cpp
    Dios santo

--- Módulo Cuenta/acc_handler.cpp
+++ Módulo Cuenta/acc_handler.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<regex>
+#include<stdio.h>
+#include<stdlib.h>
+#include<cstring>
 using namespace std;
+
+FILE *ptrF;
 
 bool validacion_email(const string& email) {
     const regex patron_email(R"((\w+)(\.\w+)*@(\w+)(\.\w+)+)");
@@ -13,12 +18,93 @@ bool validacion_numero(const string& numero) {
 }
 
 bool validacion_nombre(const string& nombre) {
-    const regex patron_nombre(R"(^[A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+$)");
+    const regex patron_nombre(R"(^[A-Z][a-z]*(\s[A-Z][a-z]*)*$)");
     return regex_match(nombre, patron_nombre);
 }
 
+void update(){
+    
+}
+void log_in() {
+    string email, email_v;
+    int opcion;
+    cout << "Ingrese su correo electronico --> ";
+    cin >> email;
+
+    if ((ptrF = fopen("cuenta_chiqui", "r")) == NULL) {
+        cout << "Error al abrir el archivo" << endl;
+    } else {
+        char buffer[256];
+        bool email_found = false;
+        
+        while (fgets(buffer, sizeof(buffer), ptrF) != NULL) {
+            string line(buffer);
+            if (line.find("Correo: ") == 0) {
+                email_v = line.substr(8); 
+                email_v.erase(email_v.find_last_not_of("\n") + 1); 
+                if (email == email_v) {
+                    email_found = true;
+                    break;
+                }
+            }
+        }
+
+        fclose(ptrF);
+
+        if (email_found) {
+            cout << "Inicio de sesion exitosa" << endl;
+            do{
+                cout << "Cuenta chiqui" << endl;
+                cout << "1.Editar cuenta" << endl;
+                cin >> opcion;
+
+                switch (opcion)
+                {
+                case 1:
+                    update();
+                    break;
+                
+                default:
+                    break;
+                }
+            }while(opcion != 1);
+        } 
+        else {
+            cout << "Correo electronico incorrecto" << endl;
+        }
+    }
+}
+
+
+void log_in_menu(){
+    int opcion1;
+    do{
+        cout << "Cuenta chiqui" << endl;
+        cout << "1. Iniciar sesion" << endl;
+        cout << "2. Volver al menu principal" << endl;
+        cin >> opcion1;
+                
+        switch(opcion1)
+        {
+            case 1:
+                log_in();
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    } while(opcion1 != 2);
+}
+
+
+struct acc_handler
+{
+    string nombre, email, direccion, numero, f_nacimiento;
+}cc;
+
+
 void cuenta_chiqui() {
-    string nombre, email, direccion, numero;
     int d_nacimiento, m_nacimiento, a_nacimiento;
     bool v_nombre = false, v_email = false, v_numero = false, v_fnacimiento = false;
     int mes_a[7] = {1, 3, 5, 7, 8, 10, 12};
@@ -26,10 +112,10 @@ void cuenta_chiqui() {
 
     cout << "Ingrese su nombre completo --> ";
     cin.ignore();  
-    getline(cin, nombre);
+    getline(cin, cc.nombre);
     cout << endl;
 
-    if(validacion_nombre(nombre)){
+    if(validacion_nombre(cc.nombre)){
         v_nombre = true;
         cout << "Nombre valido" << endl;
     }
@@ -38,10 +124,10 @@ void cuenta_chiqui() {
     }
 
     cout << "Ingrese su correo electronico --> ";
-    cin >> email;
+    cin >> cc.email;
     cout << endl;
 
-    if(validacion_email(email)){
+    if(validacion_email(cc.email)){
         v_email = true;
         cout << "Correo electronico valido" << endl;
     }
@@ -51,14 +137,14 @@ void cuenta_chiqui() {
 
     cout << "Ingrese su direccion --> ";
     cin.ignore();  
-    getline(cin, direccion);
+    getline(cin, cc.direccion);
     cout << endl;
 
     cout << "Ingrese su numero de telefono" << endl;
     cout << "+505 ";
-    cin >> numero;
+    cin >> cc.numero;
 
-    if(validacion_numero(numero)){
+    if(validacion_numero(cc.numero)){
         v_numero = true;
         cout << "Numero de telefono valido" << endl;
     }
@@ -104,11 +190,27 @@ void cuenta_chiqui() {
 
     if(v_nombre && v_email && v_numero && v_fnacimiento){
         cout << "La cuenta ha sido creada con exito" << endl;
+
+        if((ptrF= fopen("cuenta_chiqui","w"))== NULL){
+            cout << "Error al abrir el archivo" << endl;
+        }
+        else{
+            cout << "Sus datos han sido guardados exitosamente";
+            fprintf(ptrF, "Correo: %s\n", cc.email.c_str());
+            fprintf(ptrF, "Nombre: %s\n", cc.nombre.c_str());
+            fprintf(ptrF, "Direccion: %s\n", cc.direccion.c_str());
+            fprintf(ptrF, "Numero de telefono: %s\n", cc.numero.c_str());
+            fprintf(ptrF, "Fecha de nacimiento: %02d/%02d/%04d\n", d_nacimiento, m_nacimiento, a_nacimiento);
+            fclose(ptrF);
+        }
+
+        log_in_menu();
     }
     else{
         cout << "No se ha podido crear la cuenta" << endl;
     }
 }
+
 
 int main(){
     int opcion;
@@ -117,10 +219,10 @@ int main(){
     {
         cout << "Bienvenido a Banka-Smart" << endl;
         cout << "Elige una opcion" << endl;
-        cout << "Crear cuenta de ahorro --> 1" << endl;
-        cout << "Crear una cuenta chiqui--> 2" << endl;
-        cout << "Crear una cuenta universitaria --> 3" << endl; 
-        cout << "Salir --> 4" << endl;
+        cout << "1) Crear cuenta de ahorro" << endl;
+        cout << "2) Crear una cuenta chiqui" << endl;
+        cout << "3) Crear una cuenta universitaria" << endl; 
+        cout << "4) Salir" << endl;
         cin >> opcion;
 
         switch(opcion){
