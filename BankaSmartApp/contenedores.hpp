19f42807bd7fc615dca9ea4b859dd8d7fe240b1c
Kristel Geraldine Villalta Porras <165867799+krisgeraldine@users.noreply.github.com>
2024-07-08 07:49:52 -0600

    Update contenedores.hpp

--- BankaSmartApp/contenedores.hpp
+++ BankaSmartApp/contenedores.hpp
@@ -1,1 +1,11 @@
+struct Cuenta {
+    char nombre[50];
+    char email[50];
+    char direccion[100];
+    char numero[9];
+    char f_nacimiento[11];
+} cuenta;
 
+void limpiarPantalla() {
+    system("cls"); 
+}
